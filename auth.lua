aa84165ab649e2d65d6f957e5106440ef90e13b9
Master Codinger <mkcekilis@gmail.com>
2025-06-09 13:08:15 +0300

    Add files via upload

---
+++ auth.lua
@@ -0,0 +1,27 @@
+-- Allowed IP's For Servers
+local allowedIPs = {
+    "26.136.79.220", 
+	"185.221.64.240",  
+	"185.221.64.241", 
+	"45.43.143.95",  
+	"212.64.215.214", 
+	"212.64.199.128",
+	"212.64.199.9" 
+}
+
+function script.update(dt)
+
+    local serverIP = ac.getServerIP()
+    
+  
+    local isAllowed = false
+    for _, ip in ipairs(allowedIPs) do
+        if serverIP == ip then
+            isAllowed = true
+            break
+        end
+    end
+    
+
+    ac.broadcastSharedEvent("ngg_auth_status", isAllowed)
+end
