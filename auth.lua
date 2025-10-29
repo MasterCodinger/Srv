5ca3f8d06d13eb5ede01263de678d7cff4f9ebdc
Master Codinger <mkcekilis@gmail.com>
2025-10-29 22:42:00 +0300

    Update auth.lua

--- auth.lua
+++ auth.lua
@@ -3,7 +3,7 @@ local allowedIPs = {
     "26.136.79.220", 
 	"185.221.64.240",  
 	"185.221.64.241", 
-	"45.43.143.95",  
+	"104.238.21.80",  
 	"212.64.215.214", 
 	"212.64.199.128",
 	"212.64.199.9" 
@@ -25,3 +25,4 @@ function script.update(dt)
 
     ac.broadcastSharedEvent("ngg_auth_status", isAllowed)
 end
+
